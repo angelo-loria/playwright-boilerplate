2770775fe6e9b89ed8b422ad9e179d3213ee1a86
Angelo Loria <24941027+angelo-loria@users.noreply.github.com>
2023-06-21 13:22:33 -0500

    add initial spec file

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,16 +20,11 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI
-    ? [
-        ["line"],
-        ["html", { open: "never" }],
-      ]
-    : "html",
+  reporter: process.env.CI ? [["line"], ["html", { open: "never" }]] : "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: "https://www.automationexercise.com",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
