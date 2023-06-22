a4cb51bae85703ab7783988a32b733f5148a59da
Angelo Loria <24941027+angelo-loria@users.noreply.github.com>
2023-06-22 13:24:30 -0500

    Feat/update-pages (#6)

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,6 +10,7 @@ import { defineConfig, devices } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  timeout: 60_000,
   testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -27,12 +28,12 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "https://www.automationexercise.com",
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
-
+    trace: "retain-on-failure",
     /* Set custom id attribute */
     testIdAttribute: "id",
+    /* Retain video on failure */
+    video: "retain-on-failure",
   },
 
   /* Configure projects for major browsers */
