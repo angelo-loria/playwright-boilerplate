45813a05efcd0836be15e727740b8742ab9857e1
Angelo Loria <24941027+angelo-loria@users.noreply.github.com>
2023-06-22 11:13:49 -0500

    add testIdAttribute

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,9 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? [["line"], ["html", { open: "never" }]] : "html",
+  reporter: process.env.CI
+    ? [["line"], ["html", { open: "never" }]]
+    : [["html", { open: "never" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -28,6 +30,9 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
+
+    /* Set custom id attribute */
+    testIdAttribute: "id",
   },
 
   /* Configure projects for major browsers */
