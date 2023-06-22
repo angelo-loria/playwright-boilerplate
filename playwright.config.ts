f41a2ecef8ba4ea5ffa0f4cc63c580d129942843
Angelo Loria <24941027+angelo-loria@users.noreply.github.com>
2023-06-22 13:19:26 -0500

    update trace settings

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "https://www.automationexercise.com",
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    trace: "retain-on-failure",
     /* Set custom id attribute */
     testIdAttribute: "id",
     /* Retain video on failure */
