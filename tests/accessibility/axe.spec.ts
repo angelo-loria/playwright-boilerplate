3c1896bc45094bed828db7c53bf3101f02e5374d
Angelo Loria <24941027+angelo-loria@users.noreply.github.com>
2023-11-03 10:12:58 -0500

    add axe tests

--- tests/accessibility/axe.spec.ts
+++ tests/accessibility/axe.spec.ts
@@ -1,22 +1,22 @@
-// import { test, expect } from "../../pageObjects/pageFixture";
-// import AxeBuilder from "@axe-core/playwright";
+import { test, expect } from "../../pageObjects/pageFixture";
+import AxeBuilder from "@axe-core/playwright";
 
-// // https://playwright.dev/docs/accessibility-testing
+// https://playwright.dev/docs/accessibility-testing
 
-// test.describe("Accessibility Scans", () => {
-//   test.describe.configure({ retries: 0 });
+test.describe("Accessibility Scans", () => {
+  test.describe.configure({ retries: 0 });
 
-//   test("home", async ({ homePage }) => {
-//     await homePage.open();
-//     const page = homePage.page;
-//     const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
-//     expect(accessibilityScanResults.violations).toEqual([]);
-//   });
+  test("home page a11y", async ({ homePage }) => {
+    await homePage.open();
+    const page = homePage.page;
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
 
-//   test("sign up", async ({ signUpPage }) => {
-//     await signUpPage.open();
-//     const page = signUpPage.page;
-//     const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
-//     expect(accessibilityScanResults.violations).toEqual([]);
-//   });
-// });
+  test("search page a11y", async ({ searchPage }) => {
+    await searchPage.open();
+    const page = searchPage.page;
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
+});
