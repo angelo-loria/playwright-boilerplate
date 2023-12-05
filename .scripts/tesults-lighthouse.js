00634b5bd4deb23c837a8598f01c0ad3e99647ce
Angelo Loria <24941027+angelo-loria@users.noreply.github.com>
2023-12-04 22:49:31 -0600

    update path

--- .scripts/tesults-lighthouse.js
+++ .scripts/tesults-lighthouse.js
@@ -7,7 +7,7 @@ function postTesultsData() {
     let data = {
         target: `${process.env.LH_TESULTS_API_TOKEN}`,
         results: {
-            cases: JSON.parse(fs.readFileSync(`${__dirname}./tests/lighthouse/reports/cases.json`)),
+            cases: JSON.parse(fs.readFileSync(`${process.cwd()}/tests/lighthouse/reports/cases.json`)),
         },
     };
 
