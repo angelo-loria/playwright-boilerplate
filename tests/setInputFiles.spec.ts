9de49f156d101c1d4856095c3d732c86d93a575c
Angelo Loria <24941027+angelo-loria@users.noreply.github.com>
2024-03-14 10:16:01 -0500

    more commenting

--- tests/setInputFiles.spec.ts
+++ tests/setInputFiles.spec.ts
@@ -1,10 +1,6 @@
 import {test} from '@playwright/test';
 import path from 'path';
 
-test.use({
-    browserName: 'chromium',
-});
-
 test.only('should set input files', async ({ page }) => {
     await page.goto('https://www.w3schools.com/howto/howto_html_file_upload_button.asp');
     await page.getByTestId('myFile').setInputFiles(path.join(__dirname,'bogusFilePath.txt'));
